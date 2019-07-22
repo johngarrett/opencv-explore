dde9e540c6dea4d4a7abbe6cbd7d4a3079af5408
John Garrett <jgarrett@kabbage.com>
2019-07-22 10:58:14 -0400

    pedestrain tracking working

--- pedestrain-tracking.py
+++ pedestrain-tracking.py
@@ -9,37 +9,39 @@ from imutils.object_detection import non_max_suppression
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-capture = cv2.VideoCapture("https://stream-uc2-delta.dropcam.com/nexus_aac/ccb95a5e149846948179d0428ecc9304/playlist.m3u8?public=ScE29hOA5L")
+cv2.startWindowThread()
+capture = cv2.VideoCapture("videos/chaplin.mp4")
 
 while True:
     ok, frame = capture.read()
     if not ok:
         print("Cannot read video")
         sys.exit()
+
     # improve detection accuracy and speed
-    image = imutils.resize(frame, width=min(400, frame.shape[1]))
+    frame = cv2.resize(frame, (640, 480))
 
-    rectangles, weights = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    # turn to grayscale
+    #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # draw the original bboxes
-    for (x, y, w, h) in rectangles:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    #apply threshold
+    #ret, frame = cv2.threshold(frame, 80, 255, cv2.THRESH_BINARY)
+    
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
 
-    # normalize the bouding boxes
-    rectangles = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rectangles])
-    pick = non_max_suppression(rectangles, probs=None, overlapThresh=0.65)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-    # draw the final bboxes
+    # draw the final bounding boxes
     for (xA, yA, xB, yB) in pick:
-        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-        print("{} original boxes, {} after suppression".format(len(rectangles), len(pick)))
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+            print("{} original boxes, after suppression".format(len(boxes)))
     
-        timer = cv2.getTickCount()
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            timer = cv2.getTickCount()
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
- 
-        # Display result
-        cv2.imshow("Tracking", frame)
-
+            cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        
+            cv2.imshow('frame', frame)
+            cv2.waitKey(1)
 
